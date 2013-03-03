7ab08a59c8fd1fc6c163c03674eb06a9f53f972b
SAMMY BROWN <sbrown09@tufts.edu>
2013-03-03 15:36:48 -0500

    Reset

--- where.js
+++ where.js
@@ -1,33 +1,31 @@
 function main(){
-	initialize();
-//	schedule();
-}
-
-function initialize() {
-//	schedreq = new XMLHttpRequest();
-  	myLat = 0;
-  	myLong = 0;
-  	me = new google.maps.LatLng(myLat, myLong);
+  	var myLat = 0;
+  	var myLong = 0;
+  	var me = new google.maps.LatLng(myLat, myLong);
   	var mapOptions = {
 		center: me, 
 		zoom: 8, 
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
+	var map;
+	var marker;
 	var infowindow = new google.maps.InfoWindow();
 	var places;
+	initialize();
+//	schedule();
+}
+
+function initialize() {
 	map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
+	getme();
+}
+
+function getme(){
 	if (navigator.geolocation){
 			navigator.geolocation.getCurrentPosition(function(position) {
 			myLat = position.coords.latitude;
 			myLong = position.coords.longitude;
-			me = new google.maps.LatLng(myLat, myLong);
-			map.panTo(me);
-			MarkMe = new google.maps.Marker({
-					position: me,
-					title: "Here I Am!"
-				});
-				MarkMe.setMap(map);
-		//	renderMap();
+			renderMap();
 		});
 				
     }
@@ -36,11 +34,14 @@ function initialize() {
 
 
 function renderMap(){
-	MarkMe = new google.maps.Marker({
+			me = new google.maps.LatLng(myLat, myLong);
+			map.panTo(me);
+			marker = new google.maps.Marker({
 					position: me,
 					title: "Here I Am!"
 				});
-				MarkMe.setMap(map);
+				marker.setMap(map);
+
 
 				// Open info window on click of marker
 //				google.maps.event.addListener(marker, 'click', function() {
@@ -58,23 +59,23 @@ function renderMap(){
 //				service.search(schedreq, callback);
 			}
 
-function schedule() {
-	//Set up request
-	schedreq.open("GET", "http://mbtamap-cedar.herokuapp.com/mapper/redline.json ",true);
-	//Execute request
-	schedreq.send(null);
-	//Handle request (however you want)
-	schedreq.onreadystatechange = callback;
-	}
-	
-	//Handle the request
-	function callback() {
-		if (schedreq.readyState == 4 && schedreq.status == 200) {
-			schedcode = schedreq.responseText;
-			trips = JSON.parse(schedcode);
-			alert(trips);
+//function schedule() {
+//	//Set up request
+//	schedreq.open("GET", "http://mbtamap-cedar.herokuapp.com/mapper/redline.json ",true);
+//	//Execute request
+//	schedreq.send(null);
+//	//Handle request (however you want)
+//	schedreq.onreadystatechange = callback;
+//	}
+//	
+//	//Handle the request
+//	function callback() {
+//		if (schedreq.readyState == 4 && schedreq.status == 200) {
+//			schedcode = schedreq.responseText;
+//			trips = JSON.parse(schedcode);
+//			alert(trips);
 //			for(i=0;i<trips.length;i++){
 //				
 //			}
-		}
-	}
+//		}
+//	}

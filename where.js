4ad35bc8b25bd8bfb03bb76387ba327f8f4d521b
SAMMY BROWN <sbrown09@tufts.edu>
2013-03-03 16:11:07 -0500

    AHH

--- where.js
+++ where.js
@@ -35,7 +35,7 @@ function renderMap(){
 			marker = new google.maps.Marker({
 					position: me,
 					title: "Here I Am!",
-					icon: 'R.png'
+					icon: 'RT.png'
 				});
 				marker.setMap(map);
 

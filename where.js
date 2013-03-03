daf87ae2b6e5dc5426c693374535fe9a3764f8a6
SAMMY BROWN <sbrown09@tufts.edu>
2013-03-03 14:13:07 -0500

    Reset

--- where.js
+++ where.js
@@ -5,47 +5,16 @@ function main(){
 
 function initialize() {
   if (navigator.geolocation){
-  	var myLat = position.coords.latitude;
-  	var myLng = position.coords.longitude;
-	var request = new XMLHttpRequest();
-	var me = new google.maps.LatLng(myLat, myLng);
-	var myOptions = {
-			zoom: 8 ,
-			center: me,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
-	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-	var me = new google.maps.LatLng(myLat, myLng);
-	map.panTo(me);
-//				marker = new google.maps.Marker({
-//					position: me,
-//					title: "Here I Am!"
-//				});
-//				marker.setMap(map);
-
-				// Open info window on click of marker
-			//	google.maps.event.addListener(marker, 'click', function() {
-			//		infowindow.setContent(marker.title);
-			//		infowindow.open(map, marker);
-			//	});
-//
-				// Calling Google Places API
-//				var request = {
-//					location: me,
-//					radius: '500',
-//					types: ['food']
-//				};
-//				service = new google.maps.places.PlacesService(map);
-//				service.search(request, callback);
+  	myLat = position.coords.latitude;
+  	myLong = position.coords.longitude;
+  	var mapOptions = {
+		center: new google.maps.LatLng(myLat, myLong), 
+		zoom: 8, 
+		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
+	var map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
+  }
   else{alert("Geolocation is not supported by this browser.");}
-  	
-//	var mapOptions = {
-//		center: new google.maps.LatLng(myLat, myLong), 
-//		zoom: 8, 
-//		mapTypeId: google.maps.MapTypeId.ROADMAP
-//	};
-//	var map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
 }
 
 var schedreq = new XMLHttpRequest();

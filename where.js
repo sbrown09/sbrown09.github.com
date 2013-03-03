fe968e331d21f20043b6144df2114acb0af36a0f
SAMMY BROWN <sbrown09@tufts.edu>
2013-03-03 14:07:08 -0500

    Assig 3

--- where.js
+++ where.js
@@ -1,36 +1,50 @@
 function main(){
-	getLocation();
 	initialize();
 	schedule();
 }
 
-function getLocation(){
-  if (navigator.geolocation){
-    alert(navigator.geolocation.getCurrentPosition(showPosition));
-  }
-  else{alert("Geolocation is not supported by this browser.");}
-}
-
-function showPosition(position){
-  	myLat = position.coords.latitude;
-  	myLong = position.coords.longitude;
-	alert(myLat + " " + myLong)
-}
-
 function initialize() {
   if (navigator.geolocation){
-  	myLat = position.coords.latitude;
-  	myLong = position.coords.longitude;
-  }
+  	var myLat = position.coords.latitude;
+  	var myLng = position.coords.longitude;
+	var request = new XMLHttpRequest();
+	var me = new google.maps.LatLng(myLat, myLng);
+	var myOptions = {
+		zoom: 8 ,
+		center: me,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	var me = new google.maps.LatLng(myLat, myLng);
+	map.panTo(me);
+				marker = new google.maps.Marker({
+					position: me,
+					title: "Here I Am!"
+				});
+				marker.setMap(map);
+
+				// Open info window on click of marker
+			//	google.maps.event.addListener(marker, 'click', function() {
+			//		infowindow.setContent(marker.title);
+			//		infowindow.open(map, marker);
+			//	});
+//
+				// Calling Google Places API
+//				var request = {
+//					location: me,
+//					radius: '500',
+//					types: ['food']
+//				};
+//				service = new google.maps.places.PlacesService(map);
+//				service.search(request, callback);
+	};
   else{alert("Geolocation is not supported by this browser.");}
   	
-	var mapOptions = {
-		center: new google.maps.LatLng(myLat, myLong), 
-		zoom: 8, 
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var map = new google.maps.Map(document.getElementById("map_canvas"),
-    	mapOptions);
+//	var mapOptions = {
+//		center: new google.maps.LatLng(myLat, myLong), 
+//		zoom: 8, 
+//		mapTypeId: google.maps.MapTypeId.ROADMAP
+//	};
+//	var map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
 }
 
 var schedreq = new XMLHttpRequest();

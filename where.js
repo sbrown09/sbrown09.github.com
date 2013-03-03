d33252983cc974cc82ffe3c8d4ab76d6a7cef5ee
SAMMY BROWN <sbrown09@tufts.edu>
2013-03-03 14:21:49 -0500

    Reset

--- where.js
+++ where.js
@@ -4,16 +4,27 @@ function main(){
 }
 
 function initialize() {
-  if (navigator.geolocation){
-  	myLat = position.coords.latitude;
-  	myLong = position.coords.longitude;
+  	myLat = 0;
+  	myLong = 0;
+  	var me = new google.maps.LatLng(myLat, myLong);
   	var mapOptions = {
-		center: new google.maps.LatLng(myLat, myLong), 
+		center: me; 
 		zoom: 8, 
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
+	var map;
+	var marker;
+	var infowindow = new google.maps.InfoWindow();
+	var places;
 	var map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
-  }
+	if (navigator.geolocation){
+			navigator.geolocation.getCurrentPosition(function(position) {
+			myLat = position.coords.latitude;
+			myLong = position.coords.longitude;
+			alert(myLat + " " + myLong);
+//			renderMap();
+		});		
+    }
   else{alert("Geolocation is not supported by this browser.");}
 }
 

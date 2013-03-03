4af085fd743baee94c54171b122902c5ef89e6c2
SAMMY BROWN <sbrown09@tufts.edu>
2013-03-03 14:09:05 -0500

    Assig 3

--- where.js
+++ where.js
@@ -10,9 +10,10 @@ function initialize() {
 	var request = new XMLHttpRequest();
 	var me = new google.maps.LatLng(myLat, myLng);
 	var myOptions = {
-		zoom: 8 ,
-		center: me,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
+			zoom: 8 ,
+			center: me,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
 	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 	var me = new google.maps.LatLng(myLat, myLng);
 	map.panTo(me);

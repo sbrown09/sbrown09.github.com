0dd1b3f996c72705272fc1eb94ec3ab577bf1e5a
SAMMY BROWN <sbrown09@tufts.edu>
2013-03-04 13:27:05 -0500

    AHH

--- where.js
+++ where.js
@@ -38,6 +38,7 @@ function renderMap(){
 					icon: 'RT.png'
 				});
 				marker.setMap(map);
+				
 
 
 				// Open info window on click of marker
@@ -56,23 +57,25 @@ function renderMap(){
 //				service.search(schedreq, callback);
 			}
 
-//function schedule() {
-//	//Set up request
-//	schedreq.open("GET", "http://mbtamap-cedar.herokuapp.com/mapper/redline.json ",true);
-//	//Execute request
-//	schedreq.send(null);
-//	//Handle request (however you want)
-//	schedreq.onreadystatechange = callback;
-//	}
-//	
-//	//Handle the request
-//	function callback() {
-//		if (schedreq.readyState == 4 && schedreq.status == 200) {
-//			schedcode = schedreq.responseText;
-//			trips = JSON.parse(schedcode);
-//			alert(trips);
-//			for(i=0;i<trips.length;i++){
-//				
-//			}
-//		}
-//	}
+function schedule() {
+	schedreq.open("GET", "http://mbtamap-cedar.herokuapp.com/mapper/redline.json ",true);
+	schedreq.send(null);
+	schedreq.onreadystatechange = callback;
+	}
+
+	function callback() {
+		if (schedreq.readyState == 4 && schedreq.status == 200) {
+			schedcode = schedreq.responseText;
+			trips = JSON.parse(schedcode);
+			for(i=0;i<trips.length;i++){
+				spot = new google.maps.LatLng(myLat, myLong);
+				map.panTo(me);
+				marker = new google.maps.Marker({
+					position: me,
+					title: "Here I Am!",
+					icon: 'RT.png'
+				});
+				marker.setMap(map);
+			}
+		}
+	}

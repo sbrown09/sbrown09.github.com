cf2e8c3468f948315b5cb23da8b595c05ba6e7e8
SAMMY BROWN <sbrown09@tufts.edu>
2013-03-05 17:04:23 -0500

    AHH

--- where.js
+++ where.js
@@ -370,7 +370,7 @@ function waldo(){
 			        persondisp(i,'waldo.png', ppl, "Waldo ",ppl[i]['loc']['latitude'],ppl[i]['loc']['longitude']); 
 		        }
 		        if(ppl[i]['name']=="Carmen Sandiego"){
-			        persondisp(i,'carmen.png', ppl, "Carmen Sandiego ",ppl[i]['loc']['latitude'],ppl[i]['loc']['longitude']);
+			        persondisp2(i,'carmen.png', ppl, "Carmen Sandiego ",ppl[i]['loc']['latitude'],ppl[i]['loc']['longitude']);
 			    }
 	        }
 	    }
@@ -411,3 +411,36 @@ str = " is " + t + " miles away from you."
     });
 }
 
+function persondisp2(index, icon, ppl, name, lat, lon){
+Number.prototype.toRad = function() {
+   return this * Math.PI / 180;
+}
+var lat2 = myLat; 
+var lon2 = myLong;
+var farness = 100;
+var lat1 = lat; 
+var lon1 = lon;  
+var R = 6371; 
+var x1 = lat2-lat1;
+var dLat = x1.toRad();  
+var x2 = lon2-lon1;
+var dLon = x2.toRad();  
+var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
+                Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
+                Math.sin(dLon/2) * Math.sin(dLon/2);  
+var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+var t = R * c; 
+
+str = " is " + t + " miles away from you."
+
+    dude = new google.maps.LatLng(lat1, lon1);
+    marker = new google.maps.Marker({
+        map: map,
+        position: dude,
+        icon: icon
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(name + str);
+        infowindow.open(map, this);
+    });
+}

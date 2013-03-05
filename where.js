b450c69929af728e407ff4053abd34d23baea158
SAMMY BROWN <sbrown09@tufts.edu>
2013-03-05 13:36:43 -0500

    AHH

--- where.js
+++ where.js
@@ -31,59 +31,107 @@ function initialize() {
 	markers = new google.maps.Marker({position: pt, title: "Davis Station", icon: 'RT.png'});
 	Stations[1] = pt;
 	Stations[1]['name']="Davis Station";
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.3884, -71.119149);
 	markers = new google.maps.Marker({position: pt, title: "Porter Square Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[2] = pt;
 	Stations[2]['name']="Porter Square Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.373362, -71.118956);
 	markers = new google.maps.Marker({position: pt, title: "Harvard Square Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[3] = pt;
 	Stations[3]['name']="Harvard Square Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.365486, -71.103802);
 	markers = new google.maps.Marker({position: pt, title: "Central Square Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[4] = pt;
 	Stations[4]['name']="Central Square Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.36249079, -71.08617653);
 	markers = new google.maps.Marker({position: pt, title: "Kendall/MIT Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[5] = pt;
 	Stations[5]['name']="Kendall/MIT Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.361166, -71.070628);
 	markers = new google.maps.Marker({position: pt, title: "Charles/MGH Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[6] = pt;
 	Stations[6]['name']="Charles/MGH Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.35639457, -71.0624242);
 	markers = new google.maps.Marker({position: pt, title: "Park St. Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[7] = pt;
 	Stations[7]['name']="Park St. Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.355518, -71.060225);
 	markers = new google.maps.Marker({position: pt, title: "Downtown Crossing Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[8] = pt;
 	Stations[8]['name']="Downtown Crossing Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.352271, -71.055242);
 	markers = new google.maps.Marker({position: pt, title: "South Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[9] = pt;
 	Stations[9]['name']="South Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.342622, -71.056967);
 	markers = new google.maps.Marker({position: pt, title: "Broadway Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[10] = pt;
 	Stations[10]['name']="Broadway Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.330154, -71.057655);
 	markers = new google.maps.Marker({position: pt, title: "Andrew Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[11] = pt;
 	Stations[11]['name']="Andrew Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.320685, -71.052391);
 	markers = new google.maps.Marker({position: pt, title: "JFK/UMass Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[12] = pt;
 	Stations[12]['name']="JFK/UMass Station";
 	Branch[0] = pt;
@@ -91,48 +139,88 @@ function initialize() {
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.275275, -71.029583);
 	markers = new google.maps.Marker({position: pt, title: "North Quincy Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[13] = pt;
 	Stations[13]['name']="North Quincy Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.31129, -71.05333);
 	markers = new google.maps.Marker({position: pt, title: "Savin Hill Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Branch[1] = pt;
 	Branch[1]['name']="Savin Hill Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.300093, -71.061667);
 	markers = new google.maps.Marker({position: pt, title: "Fields Corner Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Branch[2] = pt;
 	Branch[2]['name']="Fields Corner Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.2665139, -71.0203369);
 	markers = new google.maps.Marker({position: pt, title: "Wollaston Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[14] = pt;
 	Stations[14]['name']="Wollaston Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.251809, -71.005409);
 	markers = new google.maps.Marker({position: pt, title: "Quincy Center Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[15] = pt;
 	Stations[15]['name']="Quincy Center Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.29312583, -71.06573796);
 	markers = new google.maps.Marker({position: pt, title: "Shawmut Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Branch[3] = pt;
 	Branch[3]['name']="Shawmut Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.233391, -71.007153);
 	markers = new google.maps.Marker({position: pt, title: "Quincy Adams Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[16] = pt;
 	Stations[16]['name']="Quincy Adams Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.284652, -71.064489);
 	markers = new google.maps.Marker({position: pt, title: "Ashmont Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Branch[4] = pt;
 	Branch[4]['name']="Ashmont Station";
 	markers.setMap(map);
 	pt = new google.maps.LatLng(42.2078543, -71.0011385);
 	markers = new google.maps.Marker({position: pt, title: "Braintree Station", icon: 'RT.png'});
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	Stations[17] = pt;
 	Stations[17]['name']="Braintree Station";
+	google.maps.event.addListener(markers, 'click', function() {
+        infowindow.setContent("BLAH");
+        infowindow.open(map, this);
+    });
 	markers.setMap(map);
 	
 	redLine = new google.maps.Polyline({
